a15117a86a4425efdd166f7f8e2d5475fec4a4e4
IlliaZyma <zyma.illia1121@vu.cdu.edu.ua>
2022-04-23 10:47:57 +0300

    lab3

--- Лаболаторна_3/Лаболаторна_3/Program.cs
+++ Лаболаторна_3/Лаболаторна_3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Name
 {
@@ -30,6 +31,61 @@ namespace Name
             {
                 return nom + "/" + denom;
             }
+        }
+        static void Task1()
+        {
+            Console.WriteLine("Enter first fraction");
+            MyFrac f1 = new MyFrac(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            Console.WriteLine("Enter second fraction");
+            MyFrac f2 = new MyFrac(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            Console.WriteLine("Choose task");
+            Console.WriteLine("ToStringWithIntegerPart - 1");
+            Console.WriteLine("DoubleValue - 2");
+            Console.WriteLine("Plus - 3");
+            Console.WriteLine("Minus - 4");
+            Console.WriteLine("Multiply - 5");
+            Console.WriteLine("Divide - 6");
+            Console.WriteLine("CalcSum1 - 7");
+            Console.WriteLine("CalcSum2 - 8");
+            Console.WriteLine();
+            Console.WriteLine("Exit - 0");
+            int a;
+            do
+            {
+                a = Int32.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        ToStringWithIntegerPart(f1);
+                        break;
+                    case 2:
+                        DoubleValue(f1);
+                        break;
+                    case 3:
+                        Plus(f1, f2);
+                        break;
+                    case 4:
+                        Minus(f1, f2);
+                        break;
+                    case 5:
+                        Multiply(f1, f2);
+                        break;
+                    case 6:
+                        Divide(f1, f2);
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter number");
+                        int n1 = Int32.Parse(Console.ReadLine());
+                        CalcSum1(n1);
+                        break;
+                    case 8:
+                        Console.WriteLine("Enter number");
+                        int n2 = Int32.Parse(Console.ReadLine());
+                        CalcSum2(n2);
+                        break;
+                }
+            }
+            while (a != 0);
         }
         static string ToStringWithIntegerPart(MyFrac f1)
         {
@@ -89,58 +145,82 @@ namespace Name
 
         static void Main()
         {
-            Console.WriteLine("Enter first fraction");
-            MyFrac f1 = new MyFrac(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
-            Console.WriteLine("Enter second fraction");
-            MyFrac f2 = new MyFrac(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
-            Console.WriteLine("Choose task");
-            Console.WriteLine("ToStringWithIntegerPart - 1");
-            Console.WriteLine("DoubleValue - 2");
-            Console.WriteLine("Plus - 3");
-            Console.WriteLine("Minus - 4");
-            Console.WriteLine("Multiply - 5");
-            Console.WriteLine("Divide - 6");
-            Console.WriteLine("CalcSum1 - 7");
-            Console.WriteLine("CalcSum2 - 8");
-            Console.WriteLine();
-            Console.WriteLine("Exit - 0");
             int a;
             do
             {
+                Console.WriteLine("Choose task:" +
+    "\n 1 - Task1" +
+    "\n 2 - Task2");
                 a = Int32.Parse(Console.ReadLine());
                 switch (a)
                 {
                     case 1:
-                        ToStringWithIntegerPart(f1);
+                        Task1();
                         break;
                     case 2:
-                        DoubleValue(f1);
+                        Student[] studs = ReadData("input.txt");
+                        runMenu(studs);
                         break;
-                    case 3:
-                        Plus(f1, f2);
-                        break;
-                    case 4:
-                        Minus(f1, f2);
-                        break;
-                    case 5:
-                        Multiply(f1, f2);
-                        break;
-                    case 6:
-                        Divide(f1, f2);
-                        break;
-                    case 7:
-                        Console.WriteLine("Enter number");
-                        int n1 = Int32.Parse(Console.ReadLine());
-                        CalcSum1(n1);
-                        break;
-                    case 8:
-                        Console.WriteLine("Enter number");
-                        int n2 = Int32.Parse(Console.ReadLine());
-                        CalcSum2(n2);
-                        break;
+                    default: break;
                 }
             }
             while (a != 0);
+        }
+
+        struct Student
+        {
+            public string surName;
+            public string firstName;
+            public string patronymic;
+            public char sex;
+            public string dateOfBirth;
+            public char mathematicsMark;
+            public char physicsMark;
+            public char informaticsMark;
+            public int scholarship;
+
+            public Student(string lineWithAllData)
+            {
+                string[] ReadyData = lineWithAllData.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                surName = ReadyData[0];
+                firstName = ReadyData[1];
+                patronymic = ReadyData[2];
+                sex = Convert.ToChar(ReadyData[3]);
+                dateOfBirth = ReadyData[4];
+                mathematicsMark = Convert.ToChar(ReadyData[5]);
+                physicsMark = Convert.ToChar(ReadyData[6]);
+                informaticsMark = Convert.ToChar(ReadyData[7]);
+                scholarship = Convert.ToInt32(ReadyData[8]);
+            }          
+        }
+        static void runMenu(Student[] studs)
+        {
+            Console.WriteLine("Вивести прізвища, імена і по батькові всіх студентів чоловічої статі, старших 18 років.");
+            for (int i = 0; i < studs.Length; i++)
+            {
+                if (studs[i].sex == 'M' || studs[i].sex == 'М' || studs[i].sex == 'Ч')
+                {
+                    var EndDate = DateTime.Now;
+                    var StarData = Convert.ToDateTime(studs[i].dateOfBirth);
+                    if ((EndDate - StarData).TotalDays/365 >= 18)
+                    {
+                        Console.Write(studs[i].surName + " ");
+                        Console.Write(studs[i].firstName + " ");
+                        Console.Write(studs[i].patronymic);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+        static Student[] ReadData(string fileName)
+        {
+            string[] ReadData = File.ReadAllLines(fileName);
+            Student[] Studs = new Student[ReadData.Length];
+            for (int i = 0; i< ReadData.Length; i++)
+            {
+                Studs[i] = new Student(ReadData[i]);
+            }
+            return Studs;
         }
     }
 }
